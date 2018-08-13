aca0960b1027670fc6bbd0e25a417e456dff89ea
yudha <yudha@YudhaPC>
2018-08-13 08:22:48 +0700

    Initial Commit

---
+++ src/Jo11/CobaOperasiString.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jo11;
+
+/**
+ *
+ * @author yudha
+ */
+public class CobaOperasiString {
+   public static void main(String[]args) 
+   {
+       String identitas = "Bhisma Yudha Saputra/ X RPL 3/ 10";
+       System.out.println("identitas : "+ identitas);
+       
+      
+           
+       
+   }
+   }
