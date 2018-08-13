05d9e5bf408d9b2db929575fa90bcd07a3ba8b54
yudha <yudha@YudhaPC>
2018-08-13 08:25:09 +0700

    Coba method length & isEmpty

--- src/Jo11/CobaOperasiString.java
+++ src/Jo11/CobaOperasiString.java
@@ -16,7 +16,18 @@ public class CobaOperasiString {
        System.out.println("identitas : "+ identitas);
        
       
-           
+       
+       String x = "Operasi";
+       System.out.println("isi variable x : " + x);
+       System.out.println("\""+ x + "\" panjang karakter = "+ x.length());
+       
+       System.out.println("x adalah kosong : " + x.isEmpty());
+       
+       String y = "";
+       System.out.println("isi variable y : "+ y);
+       System.out.println("y adalah kosong : " + y.isEmpty());
+       
+       
        
    }
    }
