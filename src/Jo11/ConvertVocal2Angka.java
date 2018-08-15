108b8ed2db7aa5a56ccf4b96a9a95900e0319818
yudha <yudha@YudhaPC>
2018-08-16 05:10:52 +0700

    Tambah Input

--- src/Jo11/ConvertVocal2Angka.java
+++ src/Jo11/ConvertVocal2Angka.java
@@ -14,8 +14,21 @@ public class ConvertVocal2Angka {
     private static void tampilJudul(String identitas) 
     {
         System.out.println("Identitas : "+ identitas);
-     
-    }}
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }    
+    private static String tampilInput() 
+    {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Masukkan Kalimat: ");
+        String kalimat = scanner.nextLine();
+        System.out.println("Kalimat Asli : " + kalimat);
+        
+        return kalimat;
+    }
+    
+   }
     
     
     
